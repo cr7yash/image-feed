bbfe78ed4831aeadd29313a2e35da9c9d39b33bc
cr7yash <yashaqua@gmail.com>
2021-04-23 14:00:50 +0530

    Fix styling and some stuff

--- src/Container/ImageFeed.js
+++ src/Container/ImageFeed.js
@@ -1,58 +1,63 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import InfiniteScroll from "react-infinite-scroll-component";
-import Images from "../components/Images/Images";
-import Loader from "../components/Loader/Loader";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Images from '../components/Images/Images';
+import Loader from '../components/Loader/Loader';
 
-import "./ImageFeed.css";
+import './ImageFeed.css';
 
 export default function ImageFeed() {
-  const [image, setImage] = useState([]);
-  const [error, setError] = useState("");
-  const [imageList, setImageList] = useState([]);
-  // console.log(imageList);
-  useEffect(() => {
-    fetchImages();
-  }, []);
+	const [image, setImage] = useState([]);
+	const [error, setError] = useState('');
+	const [imageList, setImageList] = useState([]);
+	// console.log(imageList);
+	useEffect(() => {
+		fetchImages();
+	}, []);
 
-  const fetchImages = (count = 10) => {
-    const apiRoot = "https://api.unsplash.com";
-    const accessKey = "xKPd-ca-OM64AUngVU5hYDv3I6B1EOHecqElS547_dM";
+	const fetchImages = (count = 10) => {
+		const apiRoot = 'https://api.unsplash.com';
+		const accessKey = 'xKPd-ca-OM64AUngVU5hYDv3I6B1EOHecqElS547_dM';
 
-    axios
-      .get(
-        `${apiRoot}/photos/random?client_id=${accessKey}&count=${count}&orientation=portrait`
-      )
-      .then((res) => {
-        // console.log(res.data);
-        setImage([...image, ...res.data]);
-        setImageList(res.data);
-      })
-      .catch((err) => {
-        // console.log(err);
-        setError(err);
-      });
-  };
-  if (error) {
-    return <p>Sorry there is a problem </p>;
-  }
+		axios
+			.get(
+				`${apiRoot}/photos/random?client_id=${accessKey}&count=${count}&orientation=portrait`
+			)
+			.then((res) => {
+				// console.log(res.data);
+				setImage([...image, ...res.data]);
+				setImageList(res.data);
+			})
+			.catch((err) => {
+				// console.log(err);
+				setError(err);
+			});
+	};
+	if (error) {
+		return <p>Sorry there is a problem </p>;
+	}
 
-  return (
-    <div>
-      <div className="container">
-        <InfiniteScroll
-          dataLength={image.length}
-          next={fetchImages}
-          hasMore={true}
-          loader={<Loader />}
-        >
-          <div className="imgContainer">
-            {image.map((image) => (
-              <Images imageList={imageList} url={image} id={image.id} />
-            ))}
-          </div>
-        </InfiniteScroll>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className='container'>
+				<InfiniteScroll
+					dataLength={image.length}
+					next={fetchImages}
+					hasMore={true}
+					loader={<Loader />}
+					style={{ overflow: 'hidden' }}
+				>
+					<div className='imgContainer'>
+						{image.map((image) => (
+							<Images
+								imageList={imageList}
+								url={image}
+								id={image.id}
+							/>
+						))}
+					</div>
+				</InfiniteScroll>
+			</div>
+		</div>
+	);
 }
